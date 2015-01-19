b26adf0ba20c31758aabcc8166e85aff0f581ece
Mike Stok <dev+mike@influitive.com>
2015-01-19 09:56:40 -0500

    wip test_verse_0

--- bottles/lib/bottles.rb
+++ bottles/lib/bottles.rb
@@ -2,5 +2,37 @@
 puts instructions(__FILE__)
 
 class Bottles
+  def verse(line)
+
+    <<-VERSE
+#{bottle_text(line).capitalize} of beer on the wall, #{bottle_text(line)} of beer.
+Take #{one_text(line)} down and pass it around, #{bottle_text(line - 1)} of beer on the wall.
+    VERSE
+  end
+
+#No more bottles of beer on the wall, no more bottles of beer.
+#Go to the store and buy some more, 99 bottles of beer on the wall.
+
+  private
+
+  def bottle_text(num)
+    case num
+    when 0
+      "no more bottles"
+    when 1
+      "1 bottle"
+    else
+      "#{num} bottles"
+    end
+  end
+
+  def one_text(num)
+    case num
+      when 1
+        "it"
+      else
+        "one"
+    end
+  end
 
 end
--- bottles/test/bottles_test.rb
+++ bottles/test/bottles_test.rb
@@ -17,7 +17,7 @@ VERSE
   end
 
   def test_another_verse
-    skip
+    #skip
     expected = <<-VERSE
 89 bottles of beer on the wall, 89 bottles of beer.
 Take one down and pass it around, 88 bottles of beer on the wall.
@@ -26,7 +26,7 @@ VERSE
   end
 
   def test_verse_2
-    skip
+    #skip
     expected = <<-VERSE
 2 bottles of beer on the wall, 2 bottles of beer.
 Take one down and pass it around, 1 bottle of beer on the wall.
@@ -35,7 +35,7 @@ Take one down and pass it around, 1 bottle of beer on the wall.
   end
 
   def test_verse_1
-    skip
+    #skip
     expected = <<-VERSE
 1 bottle of beer on the wall, 1 bottle of beer.
 Take it down and pass it around, no more bottles of beer on the wall.
@@ -44,7 +44,7 @@ Take it down and pass it around, no more bottles of beer on the wall.
   end
 
   def test_verse_0
-    skip
+    #skip
     expected = <<-VERSE
 No more bottles of beer on the wall, no more bottles of beer.
 Go to the store and buy some more, 99 bottles of beer on the wall.
